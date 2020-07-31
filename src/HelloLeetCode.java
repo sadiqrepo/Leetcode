210dab1b2909c365b3537fc6eaa8d649037ea67f
sadiqlogi <sadiq@logistimo.com>
2020-07-31 13:56:22 +0530

    Hello LeetCode Program

---
+++ src/HelloLeetCode.java
@@ -0,0 +1,13 @@
+/**
+ * Created by sadiq on 31/07/20.
+ */
+public class HelloLeetCode {
+
+    protected static String helloLeetCode(){
+        return "Hello LeetCode!!";
+    }
+
+    public static void main(String[] args){
+        System.out.println(helloLeetCode());
+    }
+}
